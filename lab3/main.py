8d1cfa73b21cef2fd66ac6db32eb2b30e2588a77
Miron Cristian Catalin <guytarmen@gmail.com>
2021-03-01 12:39:55 +0200

    changed lab3 to output time of execution on different inputs

--- lab3/main.py
+++ lab3/main.py
@@ -1,38 +1,60 @@
 # Subject: Empirical analysis of algorithms for obtaining Eratosthenes Sieve.
 
 import algorithms
+import time
+import csv
 
 if __name__ == "__main__":
-    while True:
-        num = int(input("What's the range of primes?\n"))
-        print("Welcome to the menu\n"
-              "1. Re-choose the range\n"
-              "2. Choose the algorithm\n"
-              "3. Exit")
-        n = int(input())
 
-        if n == 1:
-            num = int(input("How many primes do you want printed?\n"))
+    resultsList = []  # list of lists of the results for all inputs
 
-        if n == 2:
-            choice = int(input("Choose the algorithm:\n"
-                               "1.\n2.\n3.\n4.\n5.\n"))
-            if choice == 1:
-                algorithms.firstAlgorithm(num)
-            elif choice == 2:
-                algorithms.secondAlgorithm(num)
-            elif choice == 3:
-                algorithms.thirdAlgorithm(num)
-            elif choice == 4:
-                algorithms.fourthAlgorithm(num)
-            elif choice == 5:
-                algorithms.fifthAlgorithm(num)
-            else:
-                print("Wrong choice, try again!")
+    with open("timeComplexityResults.csv", "w") as tCR:
+        Writer = csv.writer(tCR, delimiter=',')
+        Writer.writerow(['range', 'firstAlgo', 'secondAlgo',
+                            'thirdAlgo', 'fourthAlgo', 'fifthAlgo'])
 
-        elif n == 3:
-            break
+    numInputBox = [100, 1000, 10000, 50000, 100000, 200000]
+    for numInput in numInputBox:
 
-        else:
-            print("Wrong choice, try again!")
+        batchOfResults = []
+        num = numInput
+        batchOfResults.append(num)
 
+        startTime = time.time()
+        algorithms.firstAlgorithm(num)
+        timeOfExecution = time.time() - startTime
+        batchOfResults.append(timeOfExecution)
+        # print("first algo done")
+
+        startTime = time.time()
+        algorithms.secondAlgorithm(num)
+        timeOfExecution = time.time() - startTime
+        batchOfResults.append(timeOfExecution)
+        # print("second algo done")
+
+        startTime = time.time()
+        algorithms.thirdAlgorithm(num)
+        timeOfExecution = time.time() - startTime
+        batchOfResults.append(timeOfExecution)
+        # print("third algo done")
+
+        startTime = time.time()
+        algorithms.fourthAlgorithm(num)
+        timeOfExecution = time.time() - startTime
+        batchOfResults.append(timeOfExecution)
+        # print("fourth algo done")
+
+        startTime = time.time()
+        algorithms.fifthAlgorithm(num)
+        timeOfExecution = time.time() - startTime
+        batchOfResults.append(timeOfExecution)
+        # print("fifth algo done")
+
+        print(batchOfResults)
+
+        resultsList.append(batchOfResults)
+
+    with open("timeComplexityResults.csv", "a") as tCR:
+        Writer = csv.writer(tCR, delimiter=',')
+        for i in range(len(resultsList)):
+            Writer.writerow(resultsList[i])
